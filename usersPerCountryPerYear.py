f8d9eb63238627c19961d5eb79ea55280e8864e2
tiaghoul <tiaguinhomartins_@hotmail.com>
2017-03-20 22:48:00 +0000

    Users per year per country is done, code clean up.

--- usersPerCountryPerYear.py
+++ usersPerCountryPerYear.py
@@ -12,21 +12,21 @@ def dealWithHTML(year_, lang_, html_):
     # and for each <a> tag, prints the country and number of contestants
 
     file_to_write = directory + "/usersPerCountryFiles/" + lang_ + "_" + year_ + ".csv"
-    # csv_file = open(file_to_write, 'w')
+    csv_file = open(file_to_write, 'w')
     print("writing to --> " + file_to_write)
 
-    # soup = BeautifulSoup(html_, "html5lib", from_encoding='utf-8')
-    # tables = soup.find_all('table')
-    # table = tables[1]
-    #
-    # for e in table.find_all('br'):
-    #     e.extract()
-    #
-    # for a in table.find_all('a'):
-    #     country_name = a.text
-    #     sibling = a.next_sibling
-    #     num_contestants = re.sub("[^0-9]", "", sibling)
-    #     csv_file.write(country_name + ", " + num_contestants + "\n")
+    soup = BeautifulSoup(html_, "html5lib", from_encoding='utf-8')
+    tables = soup.find_all('table')
+    table = tables[1]
+
+    for e in table.find_all('br'):
+        e.extract()
+
+    for a in table.find_all('a'):
+        country_name = a.text
+        sibling = a.next_sibling
+        num_contestants = re.sub("[^0-9]", "", sibling)
+        csv_file.write(country_name + ", " + num_contestants + "\n")
 
 
 def dealWithRedirected(year_, lang_):
@@ -35,29 +35,32 @@ def dealWithRedirected(year_, lang_):
     print("writing NOTHING to --> " + file_to_write_nothing)
 
 # getting all languages ever used to a list
-# allLanguagesFile = open("allLanguagesUsed.txt", "r")
-# allLangs = allLanguagesFile.readlines()
-# allLangs = [x.strip('\n') for x in allLangs]
+allLanguagesFile = open("allLanguagesUsed.txt", "r")
+allLangs = allLanguagesFile.readlines()
+allLangs = [x.strip('\n') for x in allLangs]
 
-allLangs = ['C#', 'Visual Basic', 'PL/SQL']
+allLangs = allLangs[83:]
 
-# allYears = ["09", "10", "11", "12", "13", "14", "15", "16"]
-allYears = ["15", "16"]
+allYears = ["09", "10", "11", "12", "13", "14", "15", "16"]
 
 # for every language, for every year, finds the number of contestants of each country
-
 for lang in allLangs:
-    lang_file = lang
-    lang_url = urllib.parse.quote(lang)
+    if "/" in lang:
+        lang_file = lang.replace("/", "-")
+    else:
+        lang_file = lang
+
+    # deal with '#' and white spaces, but don't wanna change '+'
+    if "+" not in lang:
+        lang_url = urllib.parse.quote(lang)
+    else:
+        lang_url = lang
     print("lang_url = " + lang_url)
     for year in allYears:
         url = "https://www.go-hero.net/jam/" + year + "/languages/" + lang_url
         html = urllib.request.urlopen(url)
-        print(html.geturl())
         if url == html.geturl():
             dealWithHTML(year, lang_file, html)
-            # print(lang + "(" + str(year) + ") --> not redirected")
         else:
             dealWithRedirected(year, lang_file)
-            # print(lang + "(" + str(year) + ") --> redirected")
     print("------------------------------------------------------------------------------------------")
