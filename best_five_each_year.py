63ea15ecbfb7474d4fa028f6529f9f8c67837f1a
Tiago Martins <tiaguinhomartins_@hotmail.com>
2017-05-04 14:02:53 +0100

    Line plot with evolution over years of five most used languages (and bar plots for each year)

---
+++ best_five_each_year.py
@@ -0,0 +1,45 @@
+import numpy as np
+import operator
+import matplotlib.pyplot as plt
+
+allYears = ["09", "10", "11", "12", "13", "14", "15", "16"]
+
+
+def make_the_plot(number_langs, sum_col, my_data, year_):
+    list_of_tuples = []
+    for i in range(number_langs):
+        name_lang = my_data[i][0]
+        total = 0.0
+
+        for index, j in enumerate(my_data[i][1]):
+            division = j / sum_col[index]
+            total = total + division
+
+        total = (total / 7) * 100
+        list_of_tuples.append((name_lang, total))
+
+    sorted_by_second = sorted(list_of_tuples, key=lambda tup: tup[1], reverse=True)
+    first_five = sorted_by_second[:5]
+    remaining_text = "Other " + str(number_langs - 5)
+
+    five_langs = [x[0] for x in first_five]
+    five_langs.append(remaining_text)
+
+    five_percentages = [x[1] for x in first_five]
+    sum_first_five = 100 - sum(x for x in five_percentages)
+    five_percentages.append(sum_first_five)
+
+    plot_values = list(range(1, 7))
+    plt.xticks(plot_values, five_langs)
+    plt.bar(plot_values, five_percentages)
+    plt.title("Year" + year_)
+    plt.savefig('/home/tiaghoul/PycharmProjects/iic-GoogleJamStudy/images/best_five_each_year/best_five_' + year_ + '.png')
+    plt.close()
+
+for year in allYears:
+    file_name = '/home/tiaghoul/PycharmProjects/iic-GoogleJamStudy/langsPerYear/langs_year_' + year + ".csv"
+    print(file_name)
+    data = np.genfromtxt(file_name, comments="?", dtype=[('lang', np.str_, 16), ('values', np.int32, (7,))], skip_header=1, delimiter=',')
+    sum_by_columns = np.nansum(data["values"], axis=0)
+    n_langs = len(data)
+    make_the_plot(n_langs, sum_by_columns, data, year)
---
+++ best_five_per_year_evo.py
@@ -0,0 +1,51 @@
+import numpy as np
+import operator
+import matplotlib.pyplot as plt
+
+
+allYears = ["09", "10", "11", "12", "13", "14", "15", "16"]
+
+five_langs = ['C', 'Java', 'Python', 'C#', 'C++']
+
+other_langs = [100] * 8
+
+plot_values = list(range(1, 9))
+plt.xticks(plot_values, allYears)
+
+
+for lang in five_langs:
+    percentages = [0] * 8
+    for index, one_year in enumerate(allYears):
+
+        file_name = '/home/tiaghoul/PycharmProjects/iic-GoogleJamStudy/langsPerYear/langs_year_' + one_year + '.csv'
+        my_data = np.genfromtxt(file_name,comments="?", dtype=[('lang', np.str_, 16), ('values', np.int32, (7,))],
+                            skip_header=1,
+                            delimiter=',')
+
+        sum_by_columns = np.nansum(my_data["values"], axis=0)
+
+        rows = np.where(my_data['lang'] == lang)
+        values_per_round = my_data[rows][0][1]
+        total_percen_year = 0.0
+        for index_round, val in enumerate(values_per_round):
+            total_percen_year = total_percen_year + (val / sum_by_columns[index_round])
+        total = (total_percen_year / 7) * 100
+        percentages[index] = total
+        other_langs[index] -= total
+
+    # if lang != 'C++':
+    plt.scatter(plot_values, percentages)
+    plt.plot(plot_values, percentages, label=lang, linewidth=2)
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.1), ncol=6, shadow=True, title="Languages:", fancybox=True)
+
+
+plt.scatter(plot_values, other_langs)
+plt.plot(plot_values, other_langs, label="Other", linewidth=2)
+plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.1), ncol=6, shadow=True, title="Languages:", fancybox=True)
+plt.savefig('/home/tiaghoul/PycharmProjects/iic-GoogleJamStudy/images/best5_evolution_years_withC++.png')
+
+
+
+
+
+
