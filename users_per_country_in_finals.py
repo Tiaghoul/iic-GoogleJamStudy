127c651647748aad88d4bfa46438f4dcb4489c6c
Tiago Martins <tiaguinhomartins_@hotmail.com>
2017-06-12 19:53:31 +0100

    Code clean up. Add report file (not finished doe..)

--- users_per_country_in_finals.py
+++ users_per_country_in_finals.py
@@ -1,11 +1,12 @@
 import numpy as np
 import operator
+import os
 
-import matplotlib.pyplot as plt
+directory = os.path.realpath(".")
+file_to_write = directory + "/analyses/countries_in_finals.csv"
 
 
 allYears = ["09", "10", "11", "12", "13", "14", "15", "16"]
-# allYears = ["14","15"]
 
 dict_times = {}
 dict_participants = {}
@@ -30,19 +31,32 @@ for year in allYears:
                 dict_participants[country] = n_users
 
 # escrever cada uma destas listas para um ficheiro
-sorted_times = sorted(dict_times.items(), key=operator.itemgetter(1), reverse=True)
-sorted_participants = sorted(dict_participants.items(), key=operator.itemgetter(1), reverse=True)
+sorted_times = sorted(dict_times.items(), key=operator.itemgetter(1), reverse=True)[:7]
+sorted_participants = sorted(dict_participants.items(), key=operator.itemgetter(1), reverse=True)[:7]
 
-print(sorted_times[:8])
-print(sorted_participants[:8])
+sorted_by_c_times = sorted(sorted_times, key=operator.itemgetter(0))
+sorted_by_c_participants = sorted(sorted_participants, key=operator.itemgetter(0))
 
-# list_times = [x[1] for x in sorted_times]
-# list_countries_part = [x[0] for x in sorted_participants]
-# list_part = [x[1] for x in sorted_participants]
-#
-# print(len(list_countries_times))
-# plot_values = list(range(1, len(list_countries_part)+1))
-# plt.xticks(plot_values, list_countries_part,  rotation='vertical')
-# plt.bar(plot_values, list_part)
+all_times = [x[1] for x in sorted_by_c_times]
+all_participants = [x[1] for x in sorted_by_c_participants]
+all_countries = [x[0] for x in sorted_by_c_participants]
+
+
+csv_file = open(file_to_write, 'w')
+for i in range(7):
+    print(all_countries[i] + " " + str(all_times[i]) + " " + str(all_participants[i]))
+    csv_file.write(all_countries[i] + " " + str(all_times[i]) + " " + str(all_participants[i]) + "\n")
+
+csv_file.close()
+
+# N = 7
+# ind = np.arange(N)
+# width = 0.27
+
+# plot_values = list(range(1, len(all_countries)+1))
+# plt.xticks(ind+width, all_countries, rotation='vertical')
+# plt.bar(ind, all_times, width)
+# plt.bar(ind+width, all_participants, width)
+# plt.ylabel('Best 7 countries')
 # plt.show()
 
